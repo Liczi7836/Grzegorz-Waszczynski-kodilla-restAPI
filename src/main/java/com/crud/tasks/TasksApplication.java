dabe819de6ab4bb8c986f6068ad3c921302fab25
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-01-25 18:45:44 +0100

    14.3 New repo.

---
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,15 @@
+package com.crud.tasks;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+
+@SpringBootApplication
+public class TasksApplication {
+
+	public static void main(String[] args) {
+		run(TasksApplication.class, args);
+	}
+
+}
