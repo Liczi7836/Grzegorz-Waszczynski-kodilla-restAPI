f7b9983b0b49d53ad7068dcd477f8bce34fb1d49
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-02-23 17:57:26 +0100

    19.3

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,47 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class SimpleEmailServiceTest {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        //Given
+        Mail mail = Mail.builder()
+                .mailTo("test@test.com")
+                .subject("Test")
+                .message("Test Message")
+                .toCc("Test2@test.com")
+                .build();
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(mail.getMailTo());
+        simpleMailMessage.setSubject(mail.getSubject());
+        simpleMailMessage.setText(mail.getMessage());
+        simpleMailMessage.setCc(mail.getToCc());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(simpleMailMessage);
+    }
+
+}
