ed71cbbb1d90c1b58d827e1335ead24fd880c4a4
Grzegorz Waszczynski <grzegorzwaszczynski.real@gmail.com>
2022-03-09 18:03:20 +0100

    21.1

---
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.trello.facade;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCard;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.mapper.CreatedTrelloCardDto;
+import com.crud.tasks.mapper.TrelloMapper;
+import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.validator.TrelloValidator;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TrelloFacade {
+    private final TrelloService trelloService;
+    private final TrelloMapper trelloMapper;
+    private final TrelloValidator trelloValidator;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloFacade.class);
+
+    public List<TrelloBoardDto> fetchTrelloBoards(){
+        List<TrelloBoard> trelloBoards = trelloMapper.mapToBoard(trelloService.fetchTrelloBoards());
+        List<TrelloBoard> filteredBoards = trelloValidator.validateTrelloBoards(trelloBoards);
+        return trelloMapper.mapToBoardsDto(filteredBoards);
+    }
+
+    public CreatedTrelloCardDto createCard(final TrelloCardDto trelloCardDto) {
+        TrelloCard trelloCard = trelloMapper.mapToCard(trelloCardDto);
+        trelloValidator.validateCard(trelloCard);
+        return trelloService.createTrelloCard(trelloMapper.mapToCardDto(trelloCard));
+    }
+}
